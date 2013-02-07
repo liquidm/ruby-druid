8ded69309f60fb51aca97c08b6412af02b1ec4af
Holger Pillmann <holger.pillmann@madvertise.de>
2013-02-07 16:09:25 +0100

    changing client to net/http

--- druid.rb
+++ druid.rb
@@ -2,7 +2,7 @@ require './lib/zoo_handler.rb'
 require './lib/query.rb'
 require './lib/response_row.rb'
 require 'json'
-require 'rest_client'
+require 'net/http'
 
 module Druid
   class Client
@@ -18,24 +18,24 @@ module Druid
       uri = @zk.data_sources[id]
       throw "data source #{id} (currently) not available" unless uri
 
-      service, data_source = id.split '/'
+      data_source = id.split('/').last
+      
       query = Query.new(data_source)
-      query.instance_exec(&block)
 
-      response = RestClient::Request.execute({
-        :method => :post,
-        :url => uri,
-        :timeout => (2 * 60 * 1000),
-        :payload => query.to_json,
-        :headers => {
-          :content_type => :json,
-          :accept => :json
-        }
-      })
+      uri = URI(@zk.data_sources[id])
+      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+      req.body = query.instance_exec(&block).to_json
 
-      throw response.to_str if response.code != 200
+      response = Net::HTTP.new(uri.host, uri.port).start do |http| 
+        http.read_timeout = (2 * 60 * 1000)
+        http.request(req)
+      end
 
-      JSON.parse(response.to_str).map{ |row| ResponseRow.new(row) }
+      if response.code == "200"
+        JSON.parse(response.body).map{ |row| ResponseRow.new(row) }
+      else
+        raise "Request failed: #{response.code}: #{response.body} "
+      end
     end
   end
 end
